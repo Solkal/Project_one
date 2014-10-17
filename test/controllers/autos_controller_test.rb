ad5aa3ac5a25cb92401de437c9694c40c441eec2
Łukasz Maślej <lukasz.maslej@gmail.com>
2014-10-17 15:45:00 +0200

    Add test to destroy auto.

--- test/controllers/autos_controller_test.rb
+++ test/controllers/autos_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class AutosControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+setup do
+	@auto = autos(:audi)
+end
+
+test "should create auto" do
+	assert_difference('Auto.count', 1) do
+	post :create, auto:{ make: @auto.make, model: @auto.model, year: @auto.year}
+	end
+	assert_redirected_to auto_path(assigns(:auto))
+end
+test "should destroy auto" do
+	assert_difference('Auto.count', -1) do
+		delete :destroy, id: @auto
+	end
+
+	assert_redirected_to autos_path
+end
+
 end
