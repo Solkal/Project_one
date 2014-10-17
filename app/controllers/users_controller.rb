49c8e4fe2ae2ab2f93d81ee47775cd25e15b6d5d
Lukasz Solka <solkalukasz@gmail.com>
2014-10-17 15:18:48 +0200

    Changed everything as a "Admin" not a "User"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+http_basic_authenticate_with name: "secret", password: "secret", except: :index
 before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 
@@ -14,7 +15,7 @@ end
 def create
   @user = User.new(user_params)
     if @user.save
-      redirect_to users_path, notice: "User was succesful created"  
+      redirect_to root_path, :notice =>  "Created!"  
     else
       render :new
     end
@@ -35,7 +36,7 @@ end
 private
 
 def user_params
-  params.require(:user).permit(:name, :surname, :login, :email, :password_hash, :password_salt)
+  params.require(:user).permit(:login, :password)
 end
 
 def set_user
--- db/migrate/20141017121018_add_column_to_users.rb
+++ db/migrate/20141017121018_add_column_to_users.rb
@@ -2,9 +2,17 @@ class AddColumnToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
     add_column :users, :surname, :string
+    add_column :users, :personal_id, :number
     add_column :users, :login, :string
     add_column :users, :email, :string
     add_column :users, :password_hash, :string
     add_column :users, :password_salt, :string
+
+    remove_column :users, :name, :string
+    remove_column :users, :surname, :string
+    remove_column :users, :personal_id, :number
+    remove_column :users, :password_hash, :string
+    remove_column :users, :password_salt, :string
+
   end
 end
