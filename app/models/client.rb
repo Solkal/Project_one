01810f2e5d022762fa8c36cc7496bdec8c0f5af6
Lukasz Solka <solkalukasz@gmail.com>
2014-10-24 10:52:47 +0200

    add few validates and fixed earilier dumb mistake

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,6 @@
 class Client < ActiveRecord::Base
-
+  validates :name, :surname, :phone, :birth_date, :personal_id, presence: true
+  validates :name, :surname, format: { with: /[A-Z{1}[a-z]{1-10}/}
+  validates :phone, format: { with: /[1-9]{1}[0-9]{8}/ }
+  has_many :rents
 end
--- test/factories.rb
+++ test/factories.rb
@@ -5,4 +5,12 @@ FactoryGirl.define do
 	year "2011"
 	license_plate "WD W315"
   end
+
+  factory :client do
+  name "Luk"
+  surname "Bobi"
+  phone "123456789"
+  birth_date "1989-01-17"
+  personal_id "89011712345"
+  end
 end
