4b303b2dc2d498cc99534b2b23801a88312f97ed
Lukasz Solka <solkalukasz@gmail.com>
2014-10-24 12:44:23 +0200

    add first test to Client Model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,10 @@
 class Client < ActiveRecord::Base
   validates :name, :surname, :phone, :birth_date, :personal_id, presence: true
-  # validates :name, :surname, format: { with: /[A-Z]{1}[a-z]{1-10}/}
-  validates :phone, format: { with: /[1-9]{1}[0-9]{8}/ }
+  validates :name, :surname, format: { with: /[A-Z]{1}[a-z]{1,10}/ }
+  #simple validate for birth date
+  validates :birth_date, format: { with: /[1-2][0,9][0-9]{2}-[0-1][0-9]-[0-3][0-9]/ }
+  validates :phone, uniqueness: true, format: { with: /[1-9]{1}[0-9]{8}/ }
+  #simple validate for personal_id
+  validates :personal_id, uniqueness: true, format: { with: /[1-9]{1}[1-9]{10}/ }
   has_many :rents
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "client attributes can't be blank" do
+    client = Client.new
+    assert client.invalid?
+    assert client.errors[:name].any?
+    assert client.errors[:surname].any?
+    assert client.errors[:phone].any?
+    assert client.errors[:birth_date].any?
+    assert client.errors[:personal_id].any?
+  end
+
 end
