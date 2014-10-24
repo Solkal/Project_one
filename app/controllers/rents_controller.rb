27803ee57756ebe2b327bfb09dabe1705ff8631c
Łukasz Maślej <lukasz.maslej@gmail.com>
2014-10-24 12:58:37 +0200

    Add search functionality for Auto model.

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -52,4 +52,3 @@ class RentsController < ApplicationController
 
 end
 
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
   root "users#index"
   resources :clients
   resources :autos
+
+  get "search/query"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
