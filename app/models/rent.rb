664c015efd2e73deafc862b6031746deea0ffa63
opazio <ola.pazio@gmail.com>
2014-10-29 15:24:27 +0100

    RENT:model & controller corrected

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,8 +1,4 @@
 class Rent < ActiveRecord::Base
   belongs_to :user
   belongs_to :auto
-  # validates: :client_id, :car_id, presence: true
-  # validates: :date_start, :date_end, presence: true
-  # daty będą rozwijane z listy (pole date cośtam) więc ta druga walidacja później nie będzie potrebna
-  # id dla obu modeli będą wpisywane klient: nazwisko i numer rejetracyjny samochodu
 end
