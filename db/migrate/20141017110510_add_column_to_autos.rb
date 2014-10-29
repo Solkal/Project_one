d6ee91258eb701249b697eb9ae5cc7f564b61679
Lukasz Solka <solkalukasz@gmail.com>
2014-10-29 15:47:41 +0100

    paginate, available to auto model

--- db/migrate/20141017110510_add_column_to_autos.rb
+++ db/migrate/20141017110510_add_column_to_autos.rb
@@ -3,5 +3,6 @@ class AddColumnToAutos < ActiveRecord::Migration
     add_column :autos, :make, :string
     add_column :autos, :model, :string
   	add_column :autos, :year, :string
+    add_column :autos, :available, :boolean
   end
 end
